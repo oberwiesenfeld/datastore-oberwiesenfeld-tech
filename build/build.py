0cc7dc5c0db37f5099e8352defdb6c9d5ff89809
oberwiesenfeld <oberwiesenfeld@users.noreply.github.com>
2021-10-17 22:42:24 +0200

    relative path

--- build/build.py
+++ build/build.py
@@ -4,12 +4,12 @@ import os
 m = '{ "motorcycles": [] }'
 y = json.loads(m)
 
-entries = os.listdir('../vehicle/source/')
+entries = os.listdir('vehicle/source/')
 for entry in entries:
     print('adding vehicle ' + entry)
-    with open('../vehicle/source/'+entry) as vehicle:
+    with open('vehicle/source/'+entry) as vehicle:
         vehicle_json = json.load(vehicle)
         y['motorcycles'].append(vehicle_json['motorcycle'])
 
-with open('../vehicle/motorcycles.json', 'w') as outfile:
+with open('vehicle/motorcycles.json', 'w') as outfile:
     json.dump(y, outfile, indent=2, ensure_ascii=False)
